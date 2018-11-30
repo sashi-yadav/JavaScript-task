1e6f699133c975c4e3e72d8ae4a1c3dd3f8a9bca
sashi-yadav <sashi.yadav@revature>
2018-11-30 19:11:28 +0530

    change

--- 9.js
+++ 9.js
@@ -1,6 +1,6 @@
 function nthLargest(myArray,number){
-	myArray.sort(function(a,b){return a-b});
-	return myArray[myArray.length-number]
+	myArray.sort(function(a,b){return b-a});
+	return myArray[number-1]
 }
-var arr= [95,21,1,4,3,6,68,72,56,100];
+var arr= [95,21,1,4,3,6,68,72,56];
 console.log(nthLargest(arr,1));
